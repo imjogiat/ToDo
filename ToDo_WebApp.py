7f0c8d7b5d363471714ade942741d60d74c711f6
Ismail <imjogiat@gmail.com>
2025-03-02 11:04:04 -0700

    Complete function to ToDoWebGUI

--- ToDo_WebApp.py
+++ ToDo_WebApp.py
@@ -7,16 +7,20 @@ def add_todo():
     new_todo = stlt.session_state["new_todo"] + "\n"
     todo_list.append(new_todo)
     tdf.write_todos(todo_list)
-    
 
+    
 stlt.title("My To-Do List App")
 stlt.subheader("Just a simple To-Do list")
 stlt.write("Please select an option below: ")
 
 
-for todo in todo_list:
-    stlt.checkbox(todo)
+for index, todo in enumerate(todo_list):
+    checked_box = stlt.checkbox(todo, key=todo)
 
-stlt.text_input("", placeholder="Add new to do item...", on_change= add_todo, key="new_todo")
+    if checked_box:
+        todo_list.pop(index)
+        tdf.write_todos(todo_list)
+        del stlt.session_state[todo]
+        stlt.rerun()
 
-
+stlt.text_input("", placeholder="Add new to do item...", on_change=add_todo, key="new_todo")
